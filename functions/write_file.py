9522315cea0daf6eed9433671b46d08c809bd3a3
MoXcz <130593875+MoXcz@users.noreply.github.com>
2025-09-16 21:15:41 -0600

    fix: catch errors when writing to file

--- functions/write_file.py
+++ functions/write_file.py
@@ -3,14 +3,22 @@ import os
 
 def write_file(working_directory, file_path, content):
     cwd = os.path.abspath(working_directory)
-    full_path = os.path.join(cwd, file_path)
+    full_path = os.path.abspath(os.path.join(cwd, file_path))
 
-    if not os.path.abspath(full_path).startswith(cwd):
+    if not full_path.startswith(cwd):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
     if not os.path.exists(os.path.dirname(full_path)):
-        os.makedirs(os.path.dirname(full_path))
+        try:
+            os.makedirs(os.path.dirname(full_path))
+        except Exception as e:
+            return f"Error: Cannot create directories: {e}"
+    if os.path.isdir(full_path):
+        return f"Error: It's a directory, expected a file"
 
-    with open(full_path, "w") as f:
-        f.write(content)
+    try:
+        with open(full_path, "w") as f:
+            f.write(content)
+    except Exception as e:
+        return f"Error: cannot write contents to file: {e}"
 
     return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
