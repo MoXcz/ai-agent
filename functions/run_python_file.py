36cf74a1307f6cdf291bccea6d545f783a8da371
MoXcz <130593875+MoXcz@users.noreply.github.com>
2025-09-16 21:48:42 -0600

    feat: run python files from within python

---
+++ functions/run_python_file.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+
+
+def run_python_file(working_directory, file_path, args=[]):
+    cwd = os.path.abspath(working_directory)
+    full_path = os.path.abspath(os.path.join(cwd, file_path))
+
+    if not full_path.startswith(cwd):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(full_path):
+        return f'Error: File "{file_path}" not found.'
+
+    ext = os.path.splitext(full_path)[1]
+    if ext != ".py":
+        return f"Error: '{file_path}' is not a Python file."
+
+    try:
+        result = subprocess.run(
+            ["python3", full_path] + args,
+            timeout=30.0,
+            capture_output=True,
+            cwd=cwd,
+            text=True,
+        )
+        if result.returncode != 0:
+            return f"Process exited with code {result.returncode}"
+        if result.stdout == "" and result.stderr == "":
+            return "No output produced"
+        return f"STDOUT: {result.stdout}\nSTDERR: {result.stderr}"
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
